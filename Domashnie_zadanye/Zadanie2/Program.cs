5a707293e3ab4e7dbddf90446119bed00bc7113c
KonstantinTr202 <kostantremasov572@gmail.com>
2025-01-27 22:47:18 +0300

    Add files via upload

---
+++ Domashnie_zadanye/Zadanie2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Zadanie2
+{
+    public abstract class Translator
+    {
+        public abstract string GetLanguage();
+    }
+    class EnglishTranslator : Translator
+    {
+        public override string GetLanguage()
+        {
+            return "Я перевеодчик с английского";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EnglishTranslator Translator = new EnglishTranslator();
+            Console.WriteLine(Translator.GetLanguage());
+        }
+    }
+}
